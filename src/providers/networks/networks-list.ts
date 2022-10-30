dee865a01aa1df95551bcd81834d49d87fb7ded2
Miroslaw Shpak <bytesbay@icloud.com>
2022-10-30 09:24:10 +0200

    save

--- src/providers/networks/networks-list.ts
+++ src/providers/networks/networks-list.ts
@@ -8,10 +8,10 @@ export const networks_list = [
     block_time: 3000,
   },
 
-  {
-    default_rpc: 'http://blockchain:8545',
-    archive_rpc: 'http://blockchain:8545',
-    key: 'ethereum',
+  { // testnet
+    default_rpc: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    archive_rpc: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    key: 'goerli',
     type: 'evm',
     block_time: 10000,
   },
