0ed4892f0b1c89c3124c5cbd65c4dc18093edbe6
Miroslaw Shpak <bytesbay@icloud.com>
2022-11-05 20:08:39 +0200

    save

--- src/libs/sich/methods/get-gas-price.ts
+++ src/libs/sich/methods/get-gas-price.ts
@@ -7,7 +7,7 @@ export async function getGasPrice(this: Sich, network: ISichNetwork) {
 
   const provider = new Ethers.providers.JsonRpcProvider(network.rpc);
 
-  const price = (await provider.getFeeData()).gasPrice;
+  const price = (await provider.getFeeData()).maxFeePerGas;
 
   return {
     gas_price_wei: price,
