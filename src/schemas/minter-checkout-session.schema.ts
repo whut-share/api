9c3f9461f4f1e66fdc7143288e59016a040ecf30
Miroslaw Shpak <bytesbay@icloud.com>
2022-12-24 20:57:46 +0200

    save

--- src/schemas/minter-checkout-session.schema.ts
+++ src/schemas/minter-checkout-session.schema.ts
@@ -145,7 +145,7 @@ export class MinterCheckoutSession extends BaseClass {
 
   @Field()
   public get url(): string {
-    return `${process.env['DA_CHECKOUT_URL']}/${this.id}`
+    return `${process.env['MINTER_CHECKOUT_URL']}/${this.id}`
   };
 
   @Field()
